b2948dbe2bb2aae9ef2e0b76bd54ae6aa66081e8
huren <420364177@qq.com>
2018-03-09 23:01:24 +0800

    vue11

---
+++ javascript/tanzhou/vue/11/02/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <vheader :seller="datas.seller"></vheader>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import vheader from './components/mods/header.vue'
+  import axios from 'axios'
+  export default {
+    data () {
+      return {
+        datas: {
+          // 商户的信息
+          seller: {},
+          // 商品数据
+          goods: [],
+          // 评论
+          ratings: []
+        }
+      }
+    },
+    mounted () {
+      axios.get('/static/data.json').then(res => {
+        this.datas.seller = res.data.seller
+        this.datas.goods = res.data.goods
+        this.datas.ratings = res.data.ratings
+      })
+    },
+    components: {
+      vheader
+    }
+  }
+  // scoped 当写上 当前这个模板和当前模板下所有的字模板 应用这个样式
+</script>
+
+<style>
+  .tab .tab-item a.router-link-active{
+    color:red
+  }
+</style>
