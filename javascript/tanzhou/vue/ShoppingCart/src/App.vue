d23d074202ae32d0fdfe5db44c2c4ad477adae2a
huren <420364177@qq.com>
2018-03-10 17:35:39 +0800

    'vue11'

---
+++ javascript/tanzhou/vue/ShoppingCart/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <vheader :seller='datas.seller'></vheader>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import vheader from './components/mods/header.vue'
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      datas: {
+        seller: {},   //用户信息
+        goods: [],   //商品数据
+        ratings: []   //评论
+      }
+    }
+  },
+  mounted () {
+    axios.get('/static/data.json').then(res => {
+      this.datas.seller = res.data.seller
+      this.datas.goods = res.data.goods
+      this.datas.ratings = res.data.ratings
+    })
+  },
+  components: {
+    vheader
+  }
+}
+</script>
+
+<style>
+  .tab .tab-item a.router-link-active{
+    color:red
+  }
+</style>
